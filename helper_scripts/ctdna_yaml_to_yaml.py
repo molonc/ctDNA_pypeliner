900f8f20c9d8191bb145ca3698b6773bf512cbd4
Patricia Ye <40039971+pattiey@users.noreply.github.com>
2019-06-05 16:55:02 -0700

    moved all helper scripts into directory

--- helper_scripts/ctdna_yaml_to_yaml.py
+++ helper_scripts/ctdna_yaml_to_yaml.py
@@ -8,8 +8,8 @@ DIR_PATH = "/Users/pye/beast_share/lustre/archive/MiSeq/MiSeq_Analysis_Files/190
 def main():
 	fastqs = [fastq for fastq in listdir(DIR_PATH) if fastq.endswith("fastq.gz")]
 	yaml_dict = {}
-	# with open('ctDNA_mapping_no_fastq.yaml', 'r') as no_fastq_yaml_file:
-	with open('test.yaml', 'r') as no_fastq_yaml_file:
+	with open('ctDNA_mapping_no_fastq.yaml', 'r') as no_fastq_yaml_file:
+	# with open('test.yaml', 'r') as no_fastq_yaml_file:
 		yaml_dict = yaml.safe_load(no_fastq_yaml_file)
 		for fastq in fastqs:
 			for pbc, pbc_dict in yaml_dict.iteritems():
@@ -21,7 +21,7 @@ def main():
 							elif re.search("_R2_", fastq):
 								sample_dict["fastq2"] = join(DIR_PATH, fastq)
 
-	with open("ctDNA_mapping.yaml", "w+") as yaml_file:
+	with open("../ctDNA_mapping.yaml", "w+") as yaml_file:
 		yaml.dump(yaml_dict, yaml_file, default_flow_style=False)
 
 
