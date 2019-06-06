0cdb57f52c98ae5034c31a762fd5bea10c84e0d8
Patricia Ye <40039971+pattiey@users.noreply.github.com>
2019-06-06 09:37:35 -0700

    refactor workflows

--- workflows/deepSNV_workflow.py
+++ workflows/deepSNV_workflow.py
@@ -5,20 +5,12 @@ import pypeliner
 import pypeliner.workflow
 import pypeliner.managed as mgd
 
-def deepSNV():
-	argparser = argparse.ArgumentParser()
-	pypeliner.app.add_arguments(argparser)
-
-	argparser.add_argument('tumour', help='tumour bam file')
-	argparser.add_argument('normal', help='normal bam file')
-	argparser.add_argument('config', help='Configuration filename')
-
-	args = vars(argparser.parse_args())
-	config = yaml.safe_load(open(args['config'], 'r'))
-	sample_id = args['tumour'].split("/")[-1].split("-")[0]
-
+def deepSNV(args):
 	pyp = pypeliner.app.Pypeline(modules=(), config=args)
 	workflow = pypeliner.workflow.Workflow()
+
+	config = yaml.safe_load(open(args['config'], 'r'))
+	sample_id = args['tumour'].split("/")[-1].split("-")[0]
 
 	workflow.commandline(
 		name='run_deepSNV',
@@ -42,4 +34,12 @@ def deepSNV():
 	pyp.run(workflow)
 
 if __name__ == '__main__':
-	deepSNV()
+	argparser = argparse.ArgumentParser()
+	pypeliner.app.add_arguments(argparser)
+
+	argparser.add_argument('tumour', help='tumour bam file')
+	argparser.add_argument('normal', help='normal bam file')
+	argparser.add_argument('config', help='Configuration filename')
+
+	args = vars(argparser.parse_args())
+	deepSNV(args)
